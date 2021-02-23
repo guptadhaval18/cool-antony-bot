9d7f35f0d899cd0cda92f0593db266e7e24cbc7a
Dhaval Gupta <guptadhaval18@gmail.com>
2021-02-23 19:49:47 +0530

    First commit

---
+++ telebot/credentials.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 18:59:51 2021
+
+@author: dhaval
+"""
+bot_token = "1591592295:AAEIkCG7mcn06fW3-kYmXzf2oUgdKme9peY"
+bot_user_name = "CoolAntonyBot"
+URL = "https://cool-antony-bot.herokuapp.com/"
