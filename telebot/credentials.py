26a5e962aa91df06eedd6dc6ff4a16e99a5e0a29
guptadhaval18 <47078156+guptadhaval18@users.noreply.github.com>
2021-02-28 02:49:20 +0530

    update api

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -5,6 +5,6 @@ Created on Tue Feb 23 18:59:51 2021
 
 @author: dhaval
 """
-bot_token = "1591592295:AAEIkCG7mcn06fW3-kYmXzf2oUgdKme9peY"
+bot_token = "1615242081:AAGqmAdveS1wBPnYt04VdrtNr4__KIvw7Ok"
 bot_user_name = "CoolAntonyBot"
 URL = "https://cool-antony-bot.herokuapp.com/"
