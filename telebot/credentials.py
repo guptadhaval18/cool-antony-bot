1ed1bd710858968fc3ed418dc9a28054871f6139
Dhaval Gupta <guptadhaval18@gmail.com>
2021-03-05 09:50:35 +0530

    Added images

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -7,4 +7,4 @@ Created on Tue Feb 23 18:59:51 2021
 """
 bot_token = "1643320398:AAEkEtAqvKRXZX2ROVlL9T2BFzOV8bojj04"
 bot_user_name = "CoolAntonyBot"
-URL = "https://f3ea0464df63.ngrok.io/"
+URL = "https://e2b9b9be52c1.ngrok.io/"
